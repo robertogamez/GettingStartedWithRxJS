2fd7c8bde16fab511ee4cb5f38b70dbdbf2e5d63
robertogamez <roberto.mb.gamez@hotmail.com>
2018-11-20 22:21:47 -0600

    Trabajando con operadores

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,62 @@
 import { Observable } from 'rxjs';
 
-let circle = document.getElementById('circle');
-let source = Observable.fromEvent(document, 'mousemove')
-    .map((e: MouseEvent) => {
-        return {
-            x: e.clientX,
-            y: e.clientY
-        }
-    }).filter(value => value.x < 500)
-        .delay(1000);
+let output = document.getElementById('output');
+let button = document.getElementById('button');
 
-function onNext(value){
-    circle.style.left = value.x;
-    circle.style.top = value.y;
+let click = Observable.fromEvent(button, 'click');
+
+function load(url: string) {
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener('load', () => {
+
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
+
+        })
+
+        xhr.open('get', url);
+        xhr.send();
+    }).retryWhen(retryStrategy({
+        attempts: 3,
+        delay: 1500
+    }));
 }
 
-source.subscribe(
-    onNext,
-    e => console.log(`error: ${e}`),
-    () => console.log('complete')
-);
+function retryStrategy({
+    attempts = 3,
+    delay = 1500
+}){
+    return function(errors) {
+        return errors.scan((acc, value) => {
+            console.log(acc, value);
+            return acc + 1;
+        }, 0)
+        .takeWhile(acc => acc < attempts)
+        .delay(delay);
+    }
+}
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement('div');
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+// click.flatMap(e => load('movies.json'))
+//     .subscribe(o => console.log(o));
+
+click.flatMap(e => load('moviess.json'))
+    .subscribe(
+        renderMovies,
+        e => console.log(`èrror: ${e}`),
+        () => console.log('complete')
+    );
